ea4f69a0544ac50f6113e1d2890dbc0a6ad8f487
stopdropandrew <stopdropandrew@gmail.com>
2010-05-14 15:58:07 -0700

    refactor out common assertions

--- test/functional/default_processors_test.rb
+++ test/functional/default_processors_test.rb
@@ -1,17 +1,26 @@
 require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
 class DefaultProcessorsTest < Test::Unit::TestCase
-  def test_png_processor_with_unoptimize_png
-    squeezer = ImageSqueeze.new(:image_processors => [ImageSqueeze::PNGCrushProcessor])
-    filename = fixtures('unoptimized_png.png')
+  def test_png_crush_processor_with_unoptimize_png
+    assert_processor_optimizes_file(ImageSqueeze::PNGCrushProcessor, 'unoptimized_png.png')
+  end
+
+  def test_png_crush_processor_with_optimized_png
+    assert_processor_doesnt_optimize_file(ImageSqueeze::PNGCrushProcessor, 'already_optimized_png.png')
+  end
+  
+  private
+  def assert_processor_optimizes_file(processor, file)
+    squeezer = ImageSqueeze.new(:processors => [processor])
+    filename = fixtures(file)
     old_size = File.size(filename)
     squeezer.squeeze!(filename)
     assert File.size(filename) < old_size
   end
-
-  def test_png_processor_with_optimized_png
-    squeezer = ImageSqueeze.new(:image_processors => [ImageSqueeze::PNGCrushProcessor])
-    filename = fixtures('already_optimized_png.png')
+  
+  def assert_processor_doesnt_optimize_file(processor, file)
+    squeezer = ImageSqueeze.new(:processors => [processor])
+    filename = fixtures(file)
     old_size = File.size(filename)
     squeezer.squeeze!(filename)
     assert File.size(filename) == old_size
